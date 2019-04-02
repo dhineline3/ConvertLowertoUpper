cb7d09e62a46427624e56ed7f445e7117f2bace1
Daniel Hineline <danielhineline3@gmail.com>
2019-04-01 21:51:15 -0400

    Final

---
+++ Practice/ConvertLowercaseToUppercase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class ConvertLowercaseToUppercase
+    {
+        static void Main(string[] args)
+        {
+            char a;
+            int b;
+
+            Console.WriteLine("Enter a letter between a-z:");
+
+            a = Convert.ToChar(Console.ReadLine());
+            b = (int)a;
+
+            if (b >= 97 && b <= 122) 
+            {
+                b = b - 32;
+
+                a = (char)b;
+                Console.WriteLine("In Upper Case Letter:" + a);
+            }
+            else
+            {
+                Console.WriteLine("You have entered an incorrect letter between a-z...");
+            }
+            Console.ReadKey();
+        }
+    }
+}
